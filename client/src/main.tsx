90cf135eb02979777706b1f7bff4ceaf568a6b6f
Pawel1894 <pawepohl@gmail.com>
2023-12-28 22:06:05 +0100

    fix linter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // eslint-disable-next-line
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode></React.StrictMode>,
 );
