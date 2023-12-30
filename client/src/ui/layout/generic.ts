1045328c946538e86ab8fd9899736cd94781b784
Pawel1894 <pawepohl@gmail.com>
2023-12-30 11:07:24 +0100

    Adding Flex component

---
+++ client/src/ui/layout/generic.ts
@@ -0,0 +1,41 @@
+export type Size = 'sm' | 'md' | 'lg' | 'none';
+
+export type ElementType = 'layout' | 'font';
+
+export type GenericProps = {
+  position?: 'relative' | 'absolute' | 'fixed' | 'sticky';
+  top?: 'auto' | '0' | '50%' | '100%';
+  left?: 'auto' | '0' | '50%' | '100%';
+  right?: 'auto' | '0' | '50%' | '100%';
+  bottom?: 'auto' | '0' | '50%' | '100%';
+  grow?: '0' | '1';
+  shrink?: '0' | '1';
+  margin?: Size | [Size, Size] | [Size, Size, Size, Size];
+  padding?: Size | [Size, Size] | [Size, Size, Size, Size];
+};
+
+const sizeToCss = (type: ElementType, size: Size) => {
+  switch (size) {
+    case 'sm':
+      return `var(--${type}-size-sm)`;
+    case 'md':
+      return `var(--${type}-size-md)`;
+    case 'lg':
+      return `var(--${type}-size-lg)`;
+    case 'none':
+      return `var(--${type}-size-none)`;
+  }
+};
+
+export const sizesToCss = (
+  type: ElementType,
+  sizes?: Size | [Size, Size] | [Size, Size, Size, Size],
+) => {
+  if (!sizes) return;
+
+  if (Array.isArray(sizes)) {
+    return sizes.map((item) => sizeToCss(type, item)).join(' ');
+  } else {
+    return sizeToCss(type, sizes);
+  }
+};
