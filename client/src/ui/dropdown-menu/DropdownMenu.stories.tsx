280b7df3e0656689610da5b18b189901f5e67ce1
Paweł Pohl <pawepohl@gmail.com>
2024-01-03 00:26:44 +0100

    Implementing generic dropdown menu component closes #18 (#22)

---
+++ client/src/ui/dropdown-menu/DropdownMenu.stories.tsx
@@ -0,0 +1,107 @@
+import type { Meta } from '@storybook/react';
+import { useArgs } from '@storybook/client-api';
+
+import { SunIcon } from '@radix-ui/react-icons';
+
+import '../../index.css';
+
+import { DropdownMenu } from './DropdownMenu';
+
+const meta = {
+  title: 'Example/DropdownMenu',
+  component: DropdownMenu,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    asChild: { table: { disable: true } },
+    modal: { table: { disable: true } },
+    forceMount: { table: { disable: true } },
+    trigger: { table: { disable: true } },
+    defaultOpen: { table: { disable: true } },
+    handleDropdownToggle: { table: { disable: true } },
+    open: {
+      action: 'open',
+      description: 'Whether or not the dropdown menu is open.',
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  args: {
+    trigger: <button>Toggle button</button>,
+    open: false,
+    handleDropdownToggle: () => console.log,
+  },
+} as Meta<typeof DropdownMenu>;
+
+export default meta;
+
+export const Primary = () => {
+  const [args, updateArgs] = useArgs<{
+    open: boolean;
+    handleDropdownToggle: () => void;
+  }>();
+
+  const handleClose = () => {
+    updateArgs({ open: false });
+  };
+
+  const handleToggle = () => {
+    updateArgs({ open: !args.open });
+  };
+
+  return (
+    <DropdownMenu
+      open={args.open}
+      handleDropdownToggle={handleToggle}
+      trigger={<button>Trigger button</button>}
+    >
+      <DropdownMenu.Content
+        sideOffset={8}
+        align="end"
+        onEscapeKeyDown={handleClose}
+        onInteractOutside={handleClose}
+      >
+        <DropdownMenu.Item disabled>Profile</DropdownMenu.Item>
+        <DropdownMenu.Item rightSlot={<span>⌘ + ⇧</span>}>
+          Settings
+        </DropdownMenu.Item>
+        <DropdownMenu.Item icon={<SunIcon />}>Logout</DropdownMenu.Item>
+        <DropdownMenu.Separator />
+        <DropdownMenu.Item>Help</DropdownMenu.Item>
+        <DropdownMenu.Label>Label</DropdownMenu.Label>
+        <DropdownMenu.Arrow />
+        <DropdownMenu.Group>
+          <DropdownMenu.Item>Report an issue</DropdownMenu.Item>
+        </DropdownMenu.Group>
+        <DropdownMenu.Sub>
+          <DropdownMenu.SubTrigger>Sub menu →</DropdownMenu.SubTrigger>
+          <DropdownMenu.Portal>
+            <DropdownMenu.SubContent>
+              <DropdownMenu.Item>Sub menu item</DropdownMenu.Item>
+              <DropdownMenu.Item>Sub menu item</DropdownMenu.Item>
+              <DropdownMenu.Arrow />
+            </DropdownMenu.SubContent>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Sub>
+        <DropdownMenu.CheckboxItem checked={true}>
+          Checkbox item
+        </DropdownMenu.CheckboxItem>
+        <DropdownMenu.CheckboxItem checked={false}>
+          Checkbox item 2
+        </DropdownMenu.CheckboxItem>
+        <DropdownMenu.RadioGroup value="one">
+          <DropdownMenu.RadioItem value="one">
+            Radio item one
+          </DropdownMenu.RadioItem>
+          <DropdownMenu.RadioItem value="two">
+            Radio item two
+          </DropdownMenu.RadioItem>
+        </DropdownMenu.RadioGroup>
+      </DropdownMenu.Content>
+    </DropdownMenu>
+  );
+};
---
+++ client/src/ui/dropdown-menu/DropdownMenu.tsx
@@ -0,0 +1,256 @@
+import * as RadixDropdownMenu from '@radix-ui/react-dropdown-menu';
+
+import { CheckIcon, DotFilledIcon } from '@radix-ui/react-icons';
+
+import styles from './DropdownMenu.module.css';
+
+type DropdownMenuContentProps = {
+  open: boolean;
+  defaultOpen?: boolean;
+  modal?: boolean;
+  handleDropdownToggle: () => void;
+  onOpenChange?: (open: boolean) => void;
+};
+
+type DropdownMenuPortalProps = {
+  forceMount?: true;
+};
+
+export type DropdownMenuProps = {
+  children: React.ReactNode;
+  trigger: React.ReactNode;
+} & DropdownMenuContentProps &
+  DropdownMenuPortalProps;
+
+/***
+ * Already contains Portal and trigger usage
+ * wrap children in <DropdownMenu.Content> to use
+ */
+export const DropdownMenu = ({
+  defaultOpen,
+  open,
+  modal = true,
+  children,
+  trigger,
+  forceMount,
+  onOpenChange,
+  handleDropdownToggle,
+}: DropdownMenuProps) => {
+  return (
+    <RadixDropdownMenu.Root
+      defaultOpen={defaultOpen}
+      open={open}
+      modal={modal}
+      onOpenChange={onOpenChange}
+    >
+      <RadixDropdownMenu.Trigger
+        className={styles.DropdownMenuTrigger}
+        onClick={handleDropdownToggle}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleDropdownToggle();
+          }
+        }}
+      >
+        {trigger}
+      </RadixDropdownMenu.Trigger>
+
+      <RadixDropdownMenu.Portal forceMount={forceMount}>
+        {children}
+      </RadixDropdownMenu.Portal>
+    </RadixDropdownMenu.Root>
+  );
+};
+
+/***
+ * use only for submenus
+ */
+DropdownMenu.Portal = function DropdownMenuPortal({
+  children,
+  ...props
+}: RadixDropdownMenu.DropdownMenuPortalProps) {
+  return (
+    <RadixDropdownMenu.Portal {...props}>{children}</RadixDropdownMenu.Portal>
+  );
+};
+
+DropdownMenu.Content = function DropdownMenuContent({
+  children,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuContentProps) {
+  return (
+    <RadixDropdownMenu.Content
+      className={`${styles.DropdownMenuContent} ${className}`}
+      {...props}
+    >
+      {children}
+    </RadixDropdownMenu.Content>
+  );
+};
+
+DropdownMenu.Arrow = function DropdownMenuArrow({
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuArrowProps) {
+  return (
+    <RadixDropdownMenu.Arrow
+      className={`${styles.DropdownMenuArrow} ${className}`}
+      {...props}
+    />
+  );
+};
+
+DropdownMenu.Item = function DropdownMenuItem({
+  children,
+  icon,
+  rightSlot,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuItemProps & {
+  icon?: React.ReactNode;
+  rightSlot?: React.ReactNode;
+}) {
+  return (
+    <RadixDropdownMenu.Item
+      className={`${styles.DropdownMenuItem} ${className}`}
+      {...props}
+    >
+      {icon ? (
+        <RadixDropdownMenu.ItemIndicator
+          forceMount={true}
+          className={styles.DropdownMenuItemIndicator}
+        >
+          {icon}
+        </RadixDropdownMenu.ItemIndicator>
+      ) : null}
+
+      {children}
+      {rightSlot ? <div className={styles.RightSlot}>{rightSlot}</div> : null}
+    </RadixDropdownMenu.Item>
+  );
+};
+
+DropdownMenu.Group = function DropdownMenuGroup({
+  children,
+  ...props
+}: RadixDropdownMenu.DropdownMenuGroupProps) {
+  return (
+    <RadixDropdownMenu.Group {...props}>{children}</RadixDropdownMenu.Group>
+  );
+};
+
+DropdownMenu.Label = function DropdownMenuLabel({
+  children,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuLabelProps) {
+  return (
+    <RadixDropdownMenu.Label
+      className={`${styles.DropdownMenuLabel} ${className}`}
+      {...props}
+    >
+      {children}
+    </RadixDropdownMenu.Label>
+  );
+};
+
+DropdownMenu.CheckboxItem = function DropdownMenuCheckboxItem({
+  children,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuCheckboxItemProps) {
+  return (
+    <RadixDropdownMenu.CheckboxItem
+      className={`${styles.DropdownMenuCheckboxItem} ${className}`}
+      {...props}
+    >
+      <RadixDropdownMenu.ItemIndicator
+        className={styles.DropdownMenuItemIndicator}
+      >
+        <CheckIcon />
+      </RadixDropdownMenu.ItemIndicator>
+      {children}
+    </RadixDropdownMenu.CheckboxItem>
+  );
+};
+
+DropdownMenu.RadioGroup = function DropdownMenuRadioGroup({
+  children,
+  ...props
+}: RadixDropdownMenu.DropdownMenuRadioGroupProps) {
+  return (
+    <RadixDropdownMenu.RadioGroup {...props}>
+      {children}
+    </RadixDropdownMenu.RadioGroup>
+  );
+};
+
+DropdownMenu.RadioItem = function DropdownMenuRadioItem({
+  children,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuRadioItemProps) {
+  return (
+    <RadixDropdownMenu.RadioItem
+      className={`${styles.DropdownMenuRadioItem} ${className}`}
+      {...props}
+    >
+      <RadixDropdownMenu.ItemIndicator
+        className={styles.DropdownMenuItemIndicator}
+      >
+        <DotFilledIcon />
+      </RadixDropdownMenu.ItemIndicator>
+      {children}
+    </RadixDropdownMenu.RadioItem>
+  );
+};
+
+DropdownMenu.Separator = function DropdownMenuSeparator({
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuSeparatorProps) {
+  return (
+    <RadixDropdownMenu.Separator
+      className={`${styles.DropdownMenuSeparator} ${className}`}
+      {...props}
+    />
+  );
+};
+
+DropdownMenu.Sub = function DropdownMenuSub({
+  children,
+  ...props
+}: RadixDropdownMenu.DropdownMenuSubProps) {
+  return <RadixDropdownMenu.Sub {...props}>{children}</RadixDropdownMenu.Sub>;
+};
+
+DropdownMenu.SubTrigger = function DropdownMenuSubTrigger({
+  children,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuSubTriggerProps) {
+  return (
+    <RadixDropdownMenu.SubTrigger
+      className={`${styles.DropdownMenuSubTrigger} ${className}`}
+      {...props}
+    >
+      {children}
+    </RadixDropdownMenu.SubTrigger>
+  );
+};
+
+DropdownMenu.SubContent = function DropdownMenuSubContent({
+  children,
+  className,
+  ...props
+}: RadixDropdownMenu.DropdownMenuSubContentProps) {
+  return (
+    <RadixDropdownMenu.SubContent
+      className={`${styles.DropdownMenuSubContent} ${className}`}
+      {...props}
+    >
+      {children}
+    </RadixDropdownMenu.SubContent>
+  );
+};
