660ab2809fe5ad95c3e61af65c8e68be88d7fa92
Pawel1894 <pawepohl@gmail.com>
2024-01-03 00:24:17 +0100

    creating storybook fro dropdown menu

--- client/src/entities/user-dropdown-menu/feature/UserDropdownMenu.tsx
+++ client/src/entities/user-dropdown-menu/feature/UserDropdownMenu.tsx
@@ -1,5 +1,11 @@
-import { SunIcon } from '@radix-ui/react-icons';
 import { useState } from 'react';
+import {
+  ExitIcon,
+  GearIcon,
+  InfoCircledIcon,
+  PersonIcon,
+} from '@radix-ui/react-icons';
+
 import { DropdownMenu } from 'ui/dropdown-menu';
 import { Flex } from 'ui/layout';
 
@@ -24,6 +30,7 @@ export const UserDropdownMenu = ({ trigger }: UserDropdownMenuProps) => {
       open={userMenuOpen}
       handleDropdownToggle={handleToggle}
       trigger={trigger}
+      modal={false}
     >
       <DropdownMenu.Content
         sideOffset={8}
@@ -31,8 +38,10 @@ export const UserDropdownMenu = ({ trigger }: UserDropdownMenuProps) => {
         onEscapeKeyDown={handleClose}
         onInteractOutside={handleClose}
       >
-        <DropdownMenu.Item disabled>Profile</DropdownMenu.Item>
+        <DropdownMenu.Arrow />
+        <DropdownMenu.Item icon={<PersonIcon />}>Profile</DropdownMenu.Item>
         <DropdownMenu.Item
+          icon={<GearIcon />}
           rightSlot={
             <Flex>
               <div>⌘</div>
@@ -42,38 +51,8 @@ export const UserDropdownMenu = ({ trigger }: UserDropdownMenuProps) => {
         >
           Settings
         </DropdownMenu.Item>
-        <DropdownMenu.Item icon={<SunIcon />}>Logout</DropdownMenu.Item>
-        <DropdownMenu.Separator />
-        <DropdownMenu.Item>Help</DropdownMenu.Item>
-        <DropdownMenu.Label>Label</DropdownMenu.Label>
-        <DropdownMenu.Arrow />
-        <DropdownMenu.Group>
-          <DropdownMenu.Item>Report an issue</DropdownMenu.Item>
-        </DropdownMenu.Group>
-        <DropdownMenu.Sub>
-          <DropdownMenu.SubTrigger>Sub menu →</DropdownMenu.SubTrigger>
-          <DropdownMenu.Portal>
-            <DropdownMenu.SubContent>
-              <DropdownMenu.Item>Sub menu item</DropdownMenu.Item>
-              <DropdownMenu.Item>Sub menu item</DropdownMenu.Item>
-              <DropdownMenu.Arrow />
-            </DropdownMenu.SubContent>
-          </DropdownMenu.Portal>
-        </DropdownMenu.Sub>
-        <DropdownMenu.CheckboxItem checked={true}>
-          Checkbox item
-        </DropdownMenu.CheckboxItem>
-        <DropdownMenu.CheckboxItem checked={false}>
-          Checkbox item 2
-        </DropdownMenu.CheckboxItem>
-        <DropdownMenu.RadioGroup value="one">
-          <DropdownMenu.RadioItem value="one">
-            Radio item one
-          </DropdownMenu.RadioItem>
-          <DropdownMenu.RadioItem value="two">
-            Radio item two
-          </DropdownMenu.RadioItem>
-        </DropdownMenu.RadioGroup>
+        <DropdownMenu.Item icon={<ExitIcon />}>Logout</DropdownMenu.Item>
+        <DropdownMenu.Item icon={<InfoCircledIcon />}>Help</DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu>
   );
