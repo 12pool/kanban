06c63b8417b24eb2099c01410abaf66fa1d62962
Pawel1894 <pawepohl@gmail.com>
2024-01-05 10:38:42 +0100

    Adding empty project dialog entity

---
+++ client/src/entities/project-dialog/feature/ProjectDialog.tsx
@@ -0,0 +1,18 @@
+import { Dialog, type DialogProps } from "ui/dialog";
+
+type ProjectDialogProps = Pick<DialogProps, 'trigger' | 'triggerClassName'>;
+
+export const ProjectDialog = ({ trigger, triggerClassName }: ProjectDialogProps) => {
+  return (
+    <Dialog
+      trigger={trigger}
+      triggerClassName={triggerClassName}
+    >
+      <Dialog.Content
+        title="Create project"
+      >
+        this will be project form 
+      </Dialog.Content>
+    </Dialog>
+  );
+};
