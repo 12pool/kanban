09717f65a925ef61672a16e6d3d5e2ba4482eacc
Pawel1894 <pawepohl@gmail.com>
2024-01-07 15:34:35 +0100

    Button component

---
+++ client/src/ui/button/Button.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import '../../reset.css';
+import '../../index.css';
+
+import { Button } from './Button';
+import { Box, Flex } from 'ui/layout';
+import { Text } from 'ui/text';
+
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+  tags: ['autodocs'],
+} as Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  argTypes: {
+    size: { table: { disable: true } },
+    variant: { table: { disable: true } },
+  },
+  args: {
+    children: 'Hello world',
+  },
+  render: ({ children }) => {
+    return (
+      <Flex direction="column" gap="xl">
+        <Flex gap="xl" align="center">
+          <Text weight="bold">Sizes</Text>
+          <Box>
+            <Text size="sm">small</Text>
+            <Button size="sm">{children}</Button>
+          </Box>
+          <Box>
+            <Text size="sm">medium (default)</Text>
+            <Button size="md">{children}</Button>
+          </Box>
+          <Box>
+            <Text size="sm">large</Text>
+            <Button size="lg">{children}</Button>
+          </Box>
+        </Flex>
+        <Flex gap="xl" align="center">
+          <Text weight="bold">Variants</Text>
+          <Box>
+            <Text size="sm">basic</Text>
+            <Button variant="basic">{children}</Button>
+          </Box>
+          <Box>
+            <Text size="sm">primary</Text>
+            <Button variant="primary">{children}</Button>
+          </Box>
+        </Flex>
+      </Flex>
+    );
+  },
+};
