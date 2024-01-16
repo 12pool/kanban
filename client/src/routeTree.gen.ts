c6f6a08c0d6ec4bc07b6c8e10c7e52e71a118a0e
Pawel1894 <pawepohl@gmail.com>
2024-01-16 15:00:39 +0100

    Ignoring routeTree for formatting

--- client/src/routeTree.gen.ts
+++ client/src/routeTree.gen.ts
@@ -1,50 +1,50 @@
-import { Route as rootRoute } from './routes/__root';
-import { Route as IndexImport } from './routes';
-import { Route as UserIndexImport } from './routes/user';
-import { Route as UserUserIdImport } from './routes/user/$userId';
-import { Route as UserUserIdSettingsImport } from './routes/user/$userId.settings';
+import { Route as rootRoute } from './routes/__root'
+import { Route as IndexImport } from './routes'
+import { Route as UserIndexImport } from './routes/user'
+import { Route as UserUserIdImport } from './routes/user/$userId'
+import { Route as UserUserIdSettingsImport } from './routes/user/$userId.settings'
 
 const IndexRoute = IndexImport.update({
   path: '/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const UserIndexRoute = UserIndexImport.update({
   path: '/user/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const UserUserIdRoute = UserUserIdImport.update({
   path: '/user/$userId',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const UserUserIdSettingsRoute = UserUserIdSettingsImport.update({
   path: '/settings',
   getParentRoute: () => UserUserIdRoute,
-} as any);
+} as any)
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
     '/': {
-      preLoaderRoute: typeof IndexImport;
-      parentRoute: typeof rootRoute;
-    };
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
+    }
     '/user/$userId': {
-      preLoaderRoute: typeof UserUserIdImport;
-      parentRoute: typeof rootRoute;
-    };
+      preLoaderRoute: typeof UserUserIdImport
+      parentRoute: typeof rootRoute
+    }
     '/user/': {
-      preLoaderRoute: typeof UserIndexImport;
-      parentRoute: typeof rootRoute;
-    };
+      preLoaderRoute: typeof UserIndexImport
+      parentRoute: typeof rootRoute
+    }
     '/user/$userId/settings': {
-      preLoaderRoute: typeof UserUserIdSettingsImport;
-      parentRoute: typeof UserUserIdImport;
-    };
+      preLoaderRoute: typeof UserUserIdSettingsImport
+      parentRoute: typeof UserUserIdImport
+    }
   }
 }
 export const routeTree = rootRoute.addChildren([
   IndexRoute,
   UserUserIdRoute.addChildren([UserUserIdSettingsRoute]),
   UserIndexRoute,
-]);
+])
