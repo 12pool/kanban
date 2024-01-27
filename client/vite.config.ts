85de181a65bac73f7c2eddf3afbfb9e2e623fdc1
Pawel1894 <pawepohl@gmail.com>
2024-01-27 15:17:33 +0100

    Removing vite checker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import checker from 'vite-plugin-checker';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    checker({
-      typescript: true,
-    }),
     TanStackRouterVite({
       routesDirectory: './src/routes',
       generatedRouteTree: './src/routeTree.gen.ts',
       routeFileIgnorePrefix: '_',
     }),
-
   ],
   server: {
     open: true,
