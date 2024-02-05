573d42787ea2a478bedd3592b6e00e381030c074
Pawel1894 <pawepohl@gmail.com>
2024-02-05 20:33:45 +0100

    exporting team providers from team module

--- backend/src/project/project.module.ts
+++ backend/src/project/project.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { TeamModule } from 'src/team/team.module';
 import { DatabaseModule } from 'src/database/database.module';
-import { teamProviders } from 'src/team/team.providers';
 
 import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
@@ -10,7 +9,7 @@ import { projectProviders } from './project.providers';
 
 @Module({
   imports: [DatabaseModule, TeamModule],
-  providers: [...projectProviders, ...teamProviders, ProjectService],
+  providers: [...projectProviders, ProjectService],
   controllers: [ProjectController],
 })
 export class ProjectModule {}
